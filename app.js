0a6c11df8fed7a6fba78bc8e96cc047ab004cadf
Ace <lukemurdock@outlook.com>
2024-04-24 19:45:46 -0500

    Complete Redesign, Navbar Redesign.

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const myCarouselElement = document.querySelector("#Carousel")
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 3000,
-  pause: false
-})
+
+
+var textArray = ["Cars", "Code", "CAD", "Robotics", "Photography", "Graphics Design"];
+var currentIndex = 0;
+var paragraph = document.getElementById("likedThingsCarousel");
+
+  function changeText() {
+      paragraph.style.opacity = 0;
+      setTimeout(function() {
+          paragraph.textContent = textArray[currentIndex];
+          currentIndex = (currentIndex + 1) % textArray.length;
+          paragraph.style.opacity = 1;
+      }, 1000); // Wait for the transition to complete before changing the text
+  }
+
+  setInterval(changeText, 5000);
